572ab777fc3652bed52ddc2d938b61e2c483857a
aasjunior <adeldivo@hotmail.com>
2023-05-14 16:53:45 -0300

    welcome-card

--- controllers/components.py
+++ controllers/components.py
@@ -7,3 +7,6 @@ def render_sidebar(name, image):
     }
 
     return render_template("components/sidebar.html", **user)
+
+def render_welcome_card(name):
+    return render_template("components/welcome-card.html", name=name)
